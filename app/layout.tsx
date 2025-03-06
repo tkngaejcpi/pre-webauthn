84a8ae7d96d300b3cef235ddac808b142d1c2992
Riley Chang <rileycki@tutamail.com>
2025-03-07 02:19:27 +0800

    use 100dvh instead of 100vh

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${interFont} ${emojiFont}`}>
-      <body className="min-h-screen flex flex-col">
+      <body className="min-h-dvh flex flex-col">
         <main className="m-4 ">{children}</main>
         <Footer />
 
